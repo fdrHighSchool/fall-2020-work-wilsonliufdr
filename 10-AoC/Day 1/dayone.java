e00eae5400eb8bf098528a3da94c75772b1266e2
wilsonliufdr <wilsonl67@nycstudents.net>
2020-12-01 09:02:42 -0500

    Day 1

---
+++ 10-AoC/Day 1/dayone.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Dayone{
+
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    String puzzle = scanner.nextLine();
+    int length = puzzle.length();
+
+    int a = 0;
+    int b = 0;
+
+    for(int i = 0; i<puzzle.length(); i++){
+      if(puzzle.charAt(i) == '('){
+        a++;
+      }
+        }
+        for(int c = 0; c<puzzle.length(); c++){
+          if(puzzle.charAt(c) == ')'){
+            b++;
+      }
+    }
+    int result = a - b;
+    System.out.println(result);
+  }
+}
